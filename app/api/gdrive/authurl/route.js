eefd6ea4ebe9fb822cea16b9fb6ffda6744d3f2d
Tharindu N <40334396+truethari@users.noreply.github.com>
2023-07-16 14:57:51 +0530

    get the code from google drive

---
+++ app/api/gdrive/authurl/route.js
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import { google } from "googleapis";
+const { OAuth2 } = google.auth;
+
+export async function POST(request) {
+  const url = request.url;
+  const host = url.split("/api")[0];
+
+  let requestBody;
+
+  if (request.headers.get("content-type") !== "application/json")
+    return NextResponse.json({ message: "Invalid content-type" }, { status: 400 });
+
+  try {
+    requestBody = await request.json();
+  } catch (error) {
+    return NextResponse.json({ message: "Invalid JSON" }, { status: 400 });
+  }
+
+  if (!requestBody.clientId)
+    return NextResponse.json({ message: "Missing client_id" }, { status: 400 });
+  if (!requestBody.clientSecret)
+    return NextResponse.json({ message: "Missing client_secret" }, { status: 400 });
+  if (!requestBody.scopes) return NextResponse.json({ message: "Missing scopes" }, { status: 400 });
+  if (!Array.isArray(requestBody.scopes))
+    return NextResponse.json({ message: "Scopes must be an array" }, { status: 400 });
+
+  const credentials = {
+    client_id: requestBody.clientId,
+    client_secret: requestBody.clientSecret,
+    redirect_uris: [`${host}/gdrive/oauth/callback`],
+  };
+
+  const oAuth2Client = new OAuth2(
+    credentials.client_id,
+    credentials.client_secret,
+    credentials.redirect_uris[0],
+  );
+
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: requestBody.scopes,
+  });
+
+  return NextResponse.json({ authUrl });
+}
--- components/drives/gdrive/formCard.jsx
+++ components/drives/gdrive/formCard.jsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -24,6 +26,24 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+
+const asyncLocalStorage = {
+  setItem(key, value) {
+    return Promise.resolve().then(function () {
+      localStorage.setItem(key, value);
+    });
+  },
+  getItem(key) {
+    return Promise.resolve().then(function () {
+      return localStorage.getItem(key);
+    });
+  },
+  removeItem(key) {
+    return Promise.resolve().then(function () {
+      localStorage.removeItem(key);
+    });
+  },
+};
 
 const columns = [
   {
@@ -158,11 +178,23 @@ export default function FormCard() {
     handleScopeError("");
   };
 
-  const handleSubmit = (e) => {
+  const [openSubmitLoading, setOpenSubmitLoading] = useState(false);
+  const [openSubmitDialog, setOpenSubmitDialog] = useState(false);
+
+  const toggleSubmitDialog = () => setOpenSubmitDialog(!openSubmitDialog);
+
+  const [authUrl, setAuthUrl] = useState("");
+  const [authUrlOpened, setAuthUrlOpened] = useState(false);
+
+  const handleAuthUrlChange = (e) => setAuthUrl(e.target.value);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (tokenName === "") return handleTokenNameError("Token name is required.");
     if (clientId === "") return handleClientIdError("Client ID is required.");
+    const domainRegex = new RegExp(/^[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}$/);
+    if (!domainRegex.test(clientId)) return handleClientIdError("Client ID must be a domain.");
     if (clientSecret === "") return handleClientSecretError("Client secret is required.");
     if (Object.keys(rowSelection).length === 0)
       return handleScopeError("Please select at least one scope.");
@@ -176,7 +208,12 @@ export default function FormCard() {
       scopes: selectedScopes,
     };
 
-    console.log(body);
+    setOpenSubmitLoading(true);
+    const response = await axios.post("/api/gdrive/authurl", body);
+    setOpenSubmitLoading(false);
+
+    setAuthUrl(response.data.authUrl);
+    toggleSubmitDialog();
   };
 
   const handleCancel = (e) => {
@@ -184,89 +221,195 @@ export default function FormCard() {
     router.back();
   };
 
+  const [authCode, setAuthCode] = useState("");
+
+  useEffect(() => {
+    if (authUrlOpened) {
+      const checkLocalStorage = async () => {
+        const value = await asyncLocalStorage.getItem("gdriveCode");
+
+        if (value) {
+          setAuthCode(value);
+          await asyncLocalStorage.removeItem("gdriveCode");
+          return;
+        }
+        setTimeout(checkLocalStorage, 1000);
+      };
+
+      checkLocalStorage();
+    } else {
+      localStorage.removeItem("gdriveCode");
+    }
+  }, [authUrlOpened]);
+
+  const openUrlInNewTab = () => {
+    setAuthUrlOpened(true);
+    window.open(authUrl, "_blank", "noopener noreferrer");
+  };
+
   return (
-    <Card className="h-[80%] w-[100%] flex flex-col justify-center">
-      <CardHeader>
-        <CardTitle>Create Token</CardTitle>
-        <CardDescription>Generate your Google API token in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Token Name</Label>
-              <Input
-                id="tokenName"
-                type="text"
-                value={tokenName}
-                onChange={handleTokenNameChange}
-                error={tokenNameError !== ""}
-              />
-              {tokenNameError !== "" && <p className="text-red-500 text-sm">{tokenNameError}</p>}
-            </div>
+    <>
+      <Card className="h-[80%] w-[100%] flex flex-col justify-center">
+        <CardHeader>
+          <CardTitle>Create Token</CardTitle>
+          <CardDescription>Generate your Google API token in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Token Name</Label>
+                <Input
+                  id="tokenName"
+                  type="text"
+                  value={tokenName}
+                  onChange={handleTokenNameChange}
+                  error={tokenNameError !== ""}
+                />
+                {tokenNameError !== "" && <p className="text-red-500 text-sm">{tokenNameError}</p>}
+              </div>
 
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Client ID</Label>
-              <Input
-                id="clientId"
-                tyoe="text"
-                placeholder="Client ID of your project"
-                value={clientId}
-                onChange={handleClientIdChange}
-                error={clientIdError !== ""}
-              />
-              {clientIdError !== "" && <p className="text-red-500 text-sm">{clientIdError}</p>}
-            </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Client ID</Label>
+                <Input
+                  id="clientId"
+                  tyoe="text"
+                  placeholder="Client ID of your project"
+                  value={clientId}
+                  onChange={handleClientIdChange}
+                  error={clientIdError !== ""}
+                />
+                {clientIdError !== "" && <p className="text-red-500 text-sm">{clientIdError}</p>}
+              </div>
 
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Client Secret</Label>
-              <Input
-                id="clientSecret"
-                type="password"
-                placeholder="Client Secret of your project"
-                value={clientSecret}
-                onChange={handleClientSecretChange}
-                error={clientSecretError !== ""}
-              />
-              {clientSecretError !== "" && (
-                <p className="text-red-500 text-sm">{clientSecretError}</p>
-              )}
-            </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Client Secret</Label>
+                <Input
+                  id="clientSecret"
+                  type="password"
+                  placeholder="Client Secret of your project"
+                  value={clientSecret}
+                  onChange={handleClientSecretChange}
+                  error={clientSecretError !== ""}
+                />
+                {clientSecretError !== "" && (
+                  <p className="text-red-500 text-sm">{clientSecretError}</p>
+                )}
+              </div>
 
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="datatable">Scopes</Label>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button variant={scopeError ? "destructive" : "outline"}>Select Scopes</Button>
-                </DialogTrigger>
-                <DialogContent className="md:max-w-[500px] sm:max-h-[500px] sm:overflow-y-scroll sm:max-w-[100%] sm:max-h-[90%]">
-                  <DialogHeader>
-                    <DialogTitle>Select API Scopes</DialogTitle>
-                    <DialogDescription>
-                      Select scopes here. Click close button when you're done.
-                    </DialogDescription>
-                  </DialogHeader>
-                  <div className="grid gap-4 py-4">
-                    <DataTable
-                      id="datatable"
-                      columns={columns}
-                      data={data}
-                      rowSelection={rowSelection}
-                      onRowSelectionChange={handleRowSelectionChange}
-                    />
-                  </div>
-                </DialogContent>
-              </Dialog>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="datatable">Scopes</Label>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant={scopeError ? "destructive" : "outline"}>Select Scopes</Button>
+                  </DialogTrigger>
+                  <DialogContent className="md:max-w-[500px] sm:overflow-y-scroll sm:max-w-[100%] sm:max-h-[90%]">
+                    <DialogHeader>
+                      <DialogTitle>Select API Scopes</DialogTitle>
+                      <DialogDescription>
+                        Select scopes here. Click close button when you're done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <DataTable
+                        id="datatable"
+                        columns={columns}
+                        data={data}
+                        rowSelection={rowSelection}
+                        onRowSelectionChange={handleRowSelectionChange}
+                      />
+                    </div>
+                  </DialogContent>
+                </Dialog>
+                {scopeError !== "" && <p className="text-red-500 text-sm">{scopeError}</p>}
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline" onClick={handleCancel}>
+            Cancel
+          </Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </CardFooter>
+      </Card>
+
+      <Dialog open={openSubmitLoading} onOpenChange={setOpenSubmitLoading}>
+        <DialogContent className="md:max-w-[500px] sm:overflow-y-scroll sm:max-w-[100%] sm:max-h-[90%]">
+          <DialogHeader>
+            <DialogTitle>Loading</DialogTitle>
+            <DialogDescription>
+              Please wait while we generate your auth link. This may take a while.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div role="status">
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline" onClick={handleCancel}>
-          Cancel
-        </Button>
-        <Button onClick={handleSubmit}>Submit</Button>
-      </CardFooter>
-    </Card>
+        </DialogContent>
+      </Dialog>
+
+      <Dialog open={openSubmitDialog} onOpenChange={setOpenSubmitDialog}>
+        <DialogContent className="md:max-w-[500px] sm:overflow-y-scroll sm:max-w-[100%] sm:max-h-[90%]">
+          <DialogHeader>
+            <DialogTitle>Select API Scopes</DialogTitle>
+            <DialogDescription>
+              Select scopes here. Click close button when you're done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Auth Url</Label>
+              <Input
+                id="authUrl"
+                type="text"
+                placeholder="Auth URL of your project"
+                value={authUrl}
+                onChange={handleAuthUrlChange}
+              />
+              <p className="text-gray-500 text-sm">
+                Changing the auth url will invalidate the previous auth url.
+              </p>
+            </div>
+
+            <Button onClick={openUrlInNewTab}>Open link in new tab</Button>
+
+            {authUrlOpened && (
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Paste the code here</Label>
+                <Input
+                  id="authUrl"
+                  type="text"
+                  placeholder="Auth URL of your project"
+                  value={authCode}
+                  onChange={(e) => setAuthCode(e.target.value)}
+                  disabled={true}
+                />
+                <p className="text-gray-500 text-sm">
+                  The code will be automatically added here when you complete the auth flow.
+                </p>
+              </div>
+            )}
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
